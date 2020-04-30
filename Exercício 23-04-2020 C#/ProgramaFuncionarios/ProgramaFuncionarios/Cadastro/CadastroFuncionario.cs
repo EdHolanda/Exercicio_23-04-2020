080246302f62c066d0740bfc6899a83347ffeee4
EdHolanda <63125320+EdHolanda@users.noreply.github.com>
2020-04-29 21:20:37 -0300

    Add files via upload

---
+++ Exercício 23-04-2020 C#/ProgramaFuncionarios/ProgramaFuncionarios/Cadastro/CadastroFuncionario.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Cadastro
+{
+    class Funcionario
+    {
+        public static Hashtable Novo()
+        {
+            Console.WriteLine("Matrícula:");
+            var codigo = Console.ReadLine();
+            Console.WriteLine("Nome:");
+            var nome = Console.ReadLine();
+            Console.WriteLine("Salário:");
+            double salario = double.Parse(Console.ReadLine());
+            double vale = salario * 0.3;
+            double liquido = salario - vale;
+
+            Hashtable DadosCadastrados = new Hashtable();
+            {
+                DadosCadastrados.Add("Matrícula", codigo);
+                DadosCadastrados.Add("Nome", nome);
+                DadosCadastrados.Add("Salário", salario);
+                DadosCadastrados.Add("Vale", vale);
+                DadosCadastrados.Add("Líquido", liquido);
+            }
+
+            return (DadosCadastrados);
+
+        }
+
+      
+    }
+}
